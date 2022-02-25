00ab6f8fd5b90fc41fbe57d31a5dffe3a5d7533d
Ilya Loginov <58655234+despair101@users.noreply.github.com>
2022-02-25 17:01:29 +0700

    Update addpos&min_segment_tree.cpp

--- data_structures/addpos&min_segment_tree.cpp
+++ data_structures/addpos&min_segment_tree.cpp
@@ -1,12 +1,12 @@
-сlass SegmentTree {
+class SegmentTree {
   struct Node {
     int mx = 0;
     int add = 0;
   };
- 
+
   int size = 1;
   vector<Node> t;
- 
+
   void build(int x, int lx, int rx, const vector<int>& a) {
     if (rx - lx == 1) {
       if (lx < sz(a)) {
@@ -19,7 +19,7 @@
       t[x].mx = max(t[2 * x + 1].mx, t[2 * x + 2].mx);
     }
   }
- 
+
   void modify(int l, int r, int v, int x, int lx, int rx) {
     if (lx >= r || rx <= l) return;
     if (lx >= l && rx <= r) {
@@ -32,25 +32,25 @@
       t[x].mx = max(t[2 * x + 1].mx, t[2 * x + 2].mx) + t[x].add;
     }
   }
- 
+
   int get(int l, int r, int x, int lx, int rx) {
     if (lx >= r || rx <= l) return 0;
     if (lx >= l && rx <= r) return t[x].mx;
     int mx = (lx + rx) / 2;
     return max(get(l, r, 2 * x + 1, lx, mx), get(l, r, 2 * x + 2, mx, rx)) + t[x].add;
   }
- 
+
 public:
   SegmentTree(const vector<int>& a) {
     while (size < sz(a)) size *= 2;
     t.resize(2 * size - 1);
     build(0, 0, size, a);
   }
-  
+
   void modify(int l, int r, int v) {
     modify(l, r, v, 0, 0, size);
   }
-  
+
   int get(int l, int r) {
     return get(l, r, 0, 0, size);
   }
