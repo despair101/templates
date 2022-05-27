204a884f6861f6af750350bed3991d5283abd9f1
Ilya Loginov <58655234+despair101@users.noreply.github.com>
2022-05-27 23:53:32 +0700

    Create bit.cpp

---
+++ data_structures/bit.cpp
@@ -0,0 +1,22 @@
+template<class A> class BIT {
+    int size;
+    vector<A> t;
+
+public:
+    A get(int l, int r) {
+        A res = 0;
+        for (; r >= 0; r &= r + 1, --r) res += t[r];
+        --l;
+        for (; l >= 0; l &= l + 1, --l) res -= t[l];
+        return res;
+    }
+    
+    void add(int i, int v) {
+        for (; i < size; i |= i + 1) t[i] += v;
+    }
+
+    BIT(int n) {
+        size = n;
+        t.assign(size, 0);
+    }
+};
