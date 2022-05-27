b6d2fb2aee89b46b3b858dc3e15047c4480c50bf
Ilya Loginov <58655234+despair101@users.noreply.github.com>
2022-05-28 00:30:26 +0700

    Create mint.cpp

---
+++ mint.cpp
@@ -0,0 +1,52 @@
+template<int mod> struct Mint {
+    int x;
+
+    Mint(int x_) : x((x_ < 0 ? x_ % mod + mod : x_ % mod)) {}
+
+    friend bool operator == (Mint a, Mint b) {
+        return a.x == b.x;
+    }
+
+    friend Mint operator + (Mint a, Mint b) {
+        return (a.x + b.x >= mod ? a.x + b.x - mod : a.x + b.x);
+    }
+    friend Mint operator += (Mint& a, Mint b) {
+        return a = (a + b);
+    }
+
+    friend Mint operator - (Mint a, Mint b) {
+        return (a.x - b.x < 0 ? a.x - b.x + mod : a.x - b.x);
+    }
+    friend Mint operator -= (Mint& a, Mint b) {
+        return a = (a - b);
+    }
+
+    friend Mint operator * (Mint a, Mint b) {
+        return ll(a.x) * (b.x) % mod;
+    }
+    friend Mint operator *= (Mint& a, Mint b) {
+        return a = (a * b);
+    }
+
+    Mint rev() {
+        Mint a = this->x, res = 1;
+        int n = mod - 2;
+        while (n) {
+            if (n & 1) {
+                res *= a;
+            }
+            a *= a;
+            n >>= 1;
+        }
+        return res;
+    }
+
+    friend istream& operator >> (istream& in, Mint<mod> a) {
+        in >> a.x;
+        return in;
+    }
+    friend ostream& operator << (ostream& out, Mint<mod> a) {
+        out << a.x;
+        return out;
+    }
+};
