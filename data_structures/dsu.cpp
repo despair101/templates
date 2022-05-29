537dc29dcc8894d73c79e9af001354ab6f0b51cd
Ilya Loginov <58655234+despair101@users.noreply.github.com>
2022-05-29 18:00:51 +0700

    Create dsu.cpp

---
+++ data_structures/dsu.cpp
@@ -0,0 +1,23 @@
+class DSU {
+    vector<int> p;
+    vector<int> r;
+
+public:
+    int get(int u) {
+        return (p[u] == u ? u : p[u] = get(p[u]));
+    }
+
+    bool unite(int u, int v) {
+        u = get(u), v = get(v);
+        if (u == v) return false;
+        if (r[u] < r[v]) swap(u, v);
+        r[u] += r[v];
+        p[v] = u;
+        return true;
+    }
+
+    DSU(int n) : p(n), r(n) {
+        iota(p.begin(), p.end(), 0);
+        fill(r.begin(), r.end(), 1);
+    }
+};
