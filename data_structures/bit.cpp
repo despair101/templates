902480ce03acb1b046c1e02e0cf9e83ecca81e0a
Ilya Loginov <58655234+despair101@users.noreply.github.com>
2022-05-29 19:36:21 +0700

    Update bit.cpp

--- data_structures/bit.cpp
+++ data_structures/bit.cpp
@@ -5,19 +5,13 @@ template<class A> class BIT {
 public:
     A get(int l, int r) {
         A res = 0;
-        for (int i = r; i >= 0; i &= i + 1, --i) {
-            res += t[i];
-        }
-        for (int i = l - 1; i >= 0; i &= i + 1, --i) {
-            res -= t[i];
-        }
+        for (int i = r; i >= 0; i &= i + 1, --i) res += t[i];
+        for (int i = l - 1; i >= 0; i &= i + 1, --i) res -= t[i];
         return res;
     }
 
     void add(int i, int v) {
-        for (; i < size; i |= i + 1) {
-            t[i] += v;
-        }
+        for (; i < size; i |= i + 1) t[i] += v;
     }
 
     int lb(int sum) {
