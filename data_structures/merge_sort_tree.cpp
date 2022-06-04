2571f2b319dd9d0f14c6711fca8daedeb4bba833
Ilya Loginov <58655234+despair101@users.noreply.github.com>
2022-06-04 16:01:42 +0700

    Create merge_sort_tree.cpp

---
+++ data_structures/merge_sort_tree.cpp
@@ -0,0 +1,34 @@
+struct MergeSortTree {
+    inline int lt(int x) { return 2 * x + 1; }
+    inline int rt(int x) { return 2 * x + 2; }
+    inline int md(int lx, int rx) { return (lx + rx) / 2; }
+
+    int size;
+    vector<vector<int>> t;
+
+    void build(int x, int lx, int rx, const vector<int>& a) {
+        if (rx - lx == 1) {
+            t[x] = { a[lx] };
+        } else {
+            build(lt(x), lx, md(lx, rx), a);
+            build(rt(x), md(lx, rx), rx, a);
+            t[x].resize(t[lt(x)].size() + t[rt(x)].size());
+            merge(t[lt(x)].begin(), t[lt(x)].end(), t[rt(x)].begin(), t[rt(x)].end(), t[x].begin());
+        }
+    }
+
+    int get(int x, int lx, int rx, int l, int r, int mi, int ma) {
+        if (lx >= r || rx <= l) return 0;
+        if (lx >= l && rx <= r) return upper_bound(t[x].begin(), t[x].end(), ma) - lower_bound(t[x].begin(), t[x].end(), mi);
+        return get(lt(x), lx, md(lx, rx), l, r, mi, ma) + get(rt(x), md(lx, rx), rx, l, r, mi, ma);
+    }
+    int get(int l, int r, int mi, int ma) {
+        return get(0, 0, size, l, r, mi, ma);
+    }
+
+    MergeSortTree(const vector<int>& a) {
+        size = sz(a);
+        t.resize(4 * size);
+        build(0, 0, size, a);
+    }
+};
