b7c05813b2b317a66467e8d9bc0d0361ffe445d3
Ilya Loginov <58655234+despair101@users.noreply.github.com>
2022-06-06 21:35:25 +0700

    Update segment_tree_of_treaps.cpp

--- data_structures/segment_tree_of_treaps.cpp
+++ data_structures/segment_tree_of_treaps.cpp
@@ -1,71 +1,4 @@
-struct Node {
-  int v;
-  int y;
-  int cnt;
-  int dp;
-  Node* l;
-  Node* r;
- 
-  Node(int v_) {
-    v = v_;
-    y = rnd();
-    cnt = dp = 1;
-    l = r = nullptr;
-  }
-};
- 
-int get(Node* t) {
-  if (!t) return 0;
-  return t->dp;
-}
- 
-void relax(Node* t) {
-  t->dp = t->cnt + get(t->l) + get(t->r);
-}
- 
-Node* merge(Node* a, Node* b) {
-  if (!a) return b;
-  if (!b) return a;
-  if (a->y > b->y) {
-    a->r = merge(a->r, b);
-    relax(a);
-    return a;
-  } else {
-    b->l = merge(a, b->l);
-    relax(b);
-    return b;
-  }
-}
- 
-pair<Node*, Node*> split(Node* a, int v) {
-  // <= & >
-  if (!a) return { nullptr, nullptr };
-  if (a->v > v) {
-    auto [t1, t2] = split(a->l, v);
-    a->l = t2;
-    relax(a);
-    return { t1, a };
-  } else {
-    auto [t1, t2] = split(a->r, v);
-    a->r = t1;
-    relax(a);
-    return { a, t2 };
-  }
-}
- 
-Node* insert(Node* a, int v) {
-  auto [t1, t2] = split(a, v);
-  // t1 <= v & t2 > v
-  auto [t11, t12] = split(t1, v - 1);
-  // t11 < v & t12 == v
-  if (t12) {
-    ++t12->cnt, ++t12->dp;
-  } else {
-    t12 = new Node(v);
-  }
-  return merge(t11, merge(t12, t2));
-}
- 
+//treap: https://github.com/despair101/templates/blob/main/data_structures/treap_ordered_set.cpp
 class SegmentTreeOfTreaps {
   int size;
   vector<Node*> t;
@@ -82,12 +15,12 @@ class SegmentTreeOfTreaps {
     }
   }
  
-  int get_(int l, int r, int v, int x, int lx, int rx) {
+  int get_(int l, int r, int v, int x, int lx, int rx) { 
     int ans = 0;
     if (lx >= r || rx <= l) {
       ans = 0;
     } else if (lx >= l && rx <= r) {
-      auto [t1, t2] = split(t[x], v - 1);
+      auto [t1, t2] = split(t[x], v);
       ans = get(t1);
       t[x] = merge(t1, t2);
     } else {
