6e583a50997c5b17da70637321ecc0a3c7d617e4
Ilya Loginov <58655234+despair101@users.noreply.github.com>
2022-06-09 21:26:09 +0700

    Create hasher.cpp

---
+++ data_structures/hasher.cpp
@@ -0,0 +1,17 @@
+template<int MOD, int P> struct Hasher {
+    int size;
+    vector<Mint<MOD>> h, p;
+
+    Hasher(const string& s) : size(s.size() + 1) {
+        h.assign(size, 0);
+        p.assign(size, 1);
+        for (int i = 0; i < size - 1; ++i) {
+            h[i + 1] = h[i] * P + s[i];
+            p[i + 1] = p[i] * P;
+        }
+    }
+
+    int getHash(int l, int r) {
+        return (h[r] - h[l] * p[r - l]).x;
+    }
+};
