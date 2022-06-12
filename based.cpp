4dc1e31d13639e296f5966e4301f32cd24bc129d
Ilya Loginov <58655234+despair101@users.noreply.github.com>
2022-06-13 01:45:15 +0700

    Update based.cpp

--- based.cpp
+++ based.cpp
@@ -21,38 +21,20 @@
 
 using namespace std;
 
+#ifdef DESPAIR_101
+#include "debug_output.h"
+#include "gcc_functions.h"
+#else
+#define cerr if (false) cerr
+#define print(a)
+#endif
+
 typedef long long ll;
 typedef unsigned long long ull;
 typedef long double ld;
 
-#ifndef DESPAIR_101
-#define cerr if (false) cerr
-#endif
-
-//debug output
-template<class A> void print(A a);
-template<class A, class B> void print(pair<A, B> a);
-template<class A, class B> void print(set<A, B> a);
-template<class A, class B> void print(multiset<A, B> a);
-template<class A, class B, class C> void print(map<A, B, C> a);
-template<class A> void print(unordered_set<A> a);
-template<class A> void print(unordered_multiset<A> a);
-template<class A, class B> void print(unordered_map<A, B> a);
-template<class A> void print(vector<A> a);
-template<class A> void print(deque<A> a);
-template<class A> void print(A a) { cerr << a; }
-template<class A, class B> void print(pair<A, B> a) { cerr << "("; print(a.first); cerr << " "; print(a.second); cerr << ")"; }
-template<class A> void print(vector<A> a) { for (A b : a) { print(b); cerr << " "; }; cerr << "\n"; }
-template<class A> void print(deque<A> a) { for (A b : a) { print(b); cerr << " "; }; cerr << "\n"; }
-template<class A, class B> void print(set<A, B> a) { for (A b : a) { print(b); cerr << " "; }; cerr << "\n"; }
-template<class A, class B> void print(multiset<A, B> a) { for (A b : a) { print(b); cerr << " "; }; cerr << "\n"; }
-template<class A, class B, class C> void print(map<A, B, C> a) { for (pair<A, B> b : a) { print(b); cerr << ' '; }; cerr << "\n"; }
-template<class A> void print(unordered_set<A> a) { for (A b : a) { print(b); cerr << " "; }; cerr << "\n"; }
-template<class A> void print(unordered_multiset<A> a) { for (A b : a) { print(b); cerr << " "; }; cerr << "\n"; }
-template<class A, class B> void print(unordered_map<A, B> a) { for (pair<A, B> b : a) { print(b); cerr << " "; }; cerr << "\n"; }
-
-template<typename A> bool umax(A& x, A y) { if (y > x) { x = y; return true; } return false; }
-template<typename A> bool umin(A& x, A y) { if (y < x) { x = y; return true; } return false; }
+template<typename A> inline bool umax(A& x, A y) { if (y > x) { x = y; return true; } return false; }
+template<typename A> inline bool umin(A& x, A y) { if (y < x) { x = y; return true; } return false; }
 
 //#define int ll
 #define sz(a) int(a.size())
@@ -69,7 +51,7 @@ signed main() {
     cout << fixed << setprecision(20);
 
     int testsLimit = 1;
-    //cin >> testsLimit;
+    cin >> testsLimit;
     for (int test = 1; test <= testsLimit; ++test) {
         cerr << "_____Test #" << test << "_____\n";
         solve();
