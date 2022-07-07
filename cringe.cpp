6e4b51e66abc9ef7a342135d713a06d70282b503
Ilya Loginov <58655234+despair101@users.noreply.github.com>
2022-07-07 18:45:42 +0700

    Create cringe.cpp

---
+++ cringe.cpp
@@ -0,0 +1,18 @@
+/* pragmas */
+#pragma GCC optimize("O3")
+#pragma GCC target("avx,avx2,bmi,bmi2")
+
+
+/* allocator */
+
+const int MAXMEM = 1e8;
+char mem[MAXMEM];
+int mempos = 0;
+inline void * operator new(size_t n)
+{
+    char * cur = mem + mempos;
+    mempos += n;
+    assert(mempos <= MAXMEM);
+    return (void*)cur;
+}
+inline void operator delete(void*) { };
