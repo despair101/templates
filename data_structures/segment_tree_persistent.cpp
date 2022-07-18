9af1959405f51028e78b1874845038486f589122
Ilya Loginov <58655234+despair101@users.noreply.github.com>
2022-07-19 02:17:01 +0700

    Update segment_tree_persistent.cpp

--- data_structures/segment_tree_persistent.cpp
+++ data_structures/segment_tree_persistent.cpp
@@ -1,68 +1,63 @@
 struct Node {
-    int sum;
+    int min;
+    int max;
     int l;
     int r;
 };
-
-class PersistentSegmentTree {
-    const int N = (1 << 24);
+ 
+struct PersistentSegmentTree {
+    int n, q, m;
+ 
     vector<Node> t;
     vector<int> roots;
-
+ 
     Node newNode(int x) {
         return t[x];
     }
-
-    int add(int i, int v, int x, int lx, int rx) {
+ 
+    pair<int, int> compose(pair<int, int> fi, pair<int, int> se) {
+        return { std::min(fi.first, se.first), std::max(fi.second, se.second) };
+    }
+ 
+    int change(int i, int v, int x, int lx, int rx) {
         t.push_back(newNode(x));
-        t.back().sum += v;
         int res = sz(t) - 1;
-        if (rx - lx == 1) return res;
+        if (rx - lx == 1) {
+            t[res].min = v;
+            t[res].max = v;
+            return res;
+        }
         int mx = (lx + rx) / 2;
         if (i < mx) {
-            t[res].l = add(i, v, t[x].l, lx, mx);
+            t[res].l = change(i, v, t[x].l, lx, mx);
         } else {
-            t[res].r = add(i, v, t[x].r, mx, rx);
+            t[res].r = change(i, v, t[x].r, mx, rx);
         }
+        t[res].min = min(t[t[res].l].min, t[t[res].r].min);
+        t[res].max = max(t[t[res].l].max, t[t[res].r].max);
         return res;
     }
-
-    int get(int l, int r, int x, int lx, int rx) {
-        if (lx >= l && rx <= r) return t[x].sum;
-        if (lx >= r || rx <= l) return 0;
+ 
+    pair<int, int> get(int l, int r, int x, int lx, int rx) {
+        if (lx >= l && rx <= r) return { t[x].min, t[x].max };
+        if (lx >= r || rx <= l) return { INT_MAX, INT_MIN };
         int mx = (lx + rx) / 2;
-        return get(l, r, t[x].l, lx, mx) + get(l, r, t[x].r, mx, rx);
+        return compose(get(l, r, t[x].l, lx, mx), get(l, r, t[x].r, mx, rx));
     }
-
-
-    int getk(int k, int x1, int x2, int lx, int rx) {
-        if (rx - lx == 1) return lx;
-        int mx = (lx + rx) / 2;
-        if (t[t[x2].l].sum - t[t[x1].l].sum >= k) {
-            return getk(k, t[x1].l, t[x2].l, lx, mx);
-        } else {
-            return getk(k - (t[t[x2].l].sum - t[t[x1].l].sum), t[x1].r, t[x2].r, mx, rx);
-        }
-    }
-
+ 
 public:
-    PersistentSegmentTree() {
-        t.reserve(N);
-        roots.reserve(450000);
-        t.push_back(Node{ 0, 0, 0 });
+    PersistentSegmentTree(int n_, int q_, int m_) : n(n_), q(q_), m(m_) {
+        t.push_back(Node{ INT_MAX, INT_MIN, 0, 0 });
         roots.push_back(0);
     }
-
-    void add(int i, int v) {
-        int ind = add(i, v, roots.back(), 0, N);
+ 
+    int change(int i, int v) {
+        int ind = change(i, v, roots.back(), 0, n);
         roots.push_back(ind);
+        return ind;
     }
-
-    int get(int l, int r, int op) {
-        return get(l, r, roots[op], 0, N);
-    }
-
-    int getk(int k, int x1, int x2) {
-        return getk(k, roots[x1], roots[x2], 0, N);
+ 
+    pair<int, int> get(int l, int r, int ind) {
+        return get(l, r, ind, 0, n);
     }
 };
