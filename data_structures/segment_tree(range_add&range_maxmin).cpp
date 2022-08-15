dba662dfbcd971b62d57e8bf0df37bdfe0cf7ed3
Ilya Loginov <58655234+despair101@users.noreply.github.com>
2022-08-16 02:33:44 +0700

    Update segment_tree(range_add&range_maxmin).cpp

--- data_structures/segment_tree(range_add&range_maxmin).cpp
+++ data_structures/segment_tree(range_add&range_maxmin).cpp
@@ -1,17 +1,17 @@
-struct Node {
-    int mx;
-    int mxp;
-    int mn;
-    int mnp;
-};
-
 class SegmentTree {
-    const int INF = 1e9; //TODO
-    
+public:
+    struct Node {
+        ll mx;
+        int mxp;
+        ll mn;
+        int mnp;
+    };
+
+private:
     int size;
     vector<Node> t;
-    vector<int> add;
- 
+    vector<ll> add;
+
     Node compose(Node fi, Node se, int pl) {
         Node res = {
             max(fi.mx, se.mx) + pl,
@@ -21,10 +21,10 @@ class SegmentTree {
         };
         return res;
     }
- 
+
     void build(int x, int lx, int rx) {
         if (rx - lx == 1) {
-            t[x] = { 0, lx, 0, lx, lx };
+            t[x] = { 0, lx, 0, lx };
         } else {
             int mx = (lx + rx) / 2;
             build(2 * x + 1, lx, mx);
@@ -32,7 +32,7 @@ class SegmentTree {
             t[x] = compose(t[2 * x + 1], t[2 * x + 2], add[x]);
         }
     }
- 
+
     void modify(int l, int r, int v, int x, int lx, int rx) {
         if (lx >= r || rx <= l) return;
         if (lx >= l && rx <= r) {
@@ -46,23 +46,23 @@ class SegmentTree {
             t[x] = compose(t[2 * x + 1], t[2 * x + 2], add[x]);
         }
     }
-    
+
     Node get(int l, int r, int x, int lx, int rx) {
-        if (lx >= r || rx <= l) return { -INF, N, INF, N, -1 };
+        if (lx >= r || rx <= l) return { LLONG_MIN, size, LLONG_MAX, size };
         if (lx >= l && rx <= r) return t[x];
         int mx = (lx + rx) / 2;
         return compose(get(l, r, 2 * x + 1, lx, mx), get(l, r, 2 * x + 2, mx, rx), add[x]);
     }
-    
+
 public:
     void modify(int l, int r, int v) {
         modify(l, r, v, 0, 0, size);
     }
-    
+
     Node get(int l, int r) {
         return get(l, r, 0, 0, size);
     }
-    
+
     explicit SegmentTree() {}
     explicit SegmentTree(int n) : size(n) {
         t.resize(4 * size);
