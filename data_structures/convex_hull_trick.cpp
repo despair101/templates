a58fd28bd1b357948124c0cc137720e4e84a0a1e
Ilya Loginov <58655234+despair101@users.noreply.github.com>
2022-09-27 01:28:03 +0700

    Create convex_hull_trick.cpp

---
+++ data_structures/convex_hull_trick.cpp
@@ -0,0 +1,47 @@
+inline ll div_ceil(ll a, ll b) {
+    if (b < 0) {
+        b = -b;
+        a = -a;
+    }
+
+    if (a >= 0) {
+        return (a + b - 1) / b;
+    } else {
+        return a / b;
+    }
+}
+
+struct Line {
+    ll k;
+    ll b;
+
+    ll intersect(Line y) {
+        return div_ceil(y.b - b, k - y.k);
+    }
+
+    ll operator()(int x) {
+        return k * x + b;
+    }
+};
+
+struct ConvexHull {
+    vector<Line> v;
+    vector<ll> p;
+
+    void insert(Line li) {
+        while (!v.empty() && v.back().intersect(li) <= p.back()) {
+            v.pop_back();
+            p.pop_back();
+        }
+        if (v.empty()) {
+            p.push_back(0); //min X
+        } else {
+            p.push_back(v.back().intersect(li));
+        }
+        v.push_back(li);
+    }
+
+    int getMin(int x) {
+        return v[upper_bound(p.begin(), p.end(), x) - p.begin() - 1](x);
+    }
+};
