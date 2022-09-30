8624c664c58de3e9c626367db9791b5e6efbd904
Ilya Loginov <58655234+despair101@users.noreply.github.com>
2022-09-30 23:48:32 +0700

    Update li-chao_tree.cpp

--- data_structures/li-chao_tree.cpp
+++ data_structures/li-chao_tree.cpp
@@ -1,30 +1,9 @@
-int div_ceil(int a, int b) {
-    if (b < 0) {
-        a = -a;
-        b = -b;
-    }
-
-    if (a >= 0) {
-        return (a + b - 1) / b;
-    } else {
-        return a / b;
-    }
-}
-
 struct Line {
     int k;
     int b;
 
-    ld intersect(Line l) {
-        return ld(l.b - b) / (k - l.k);
-    }
-
     int operator()(int x) {
         return k * x + b;
-    }
-
-    bool operator < (Line l) const {
-        return make_pair(k, b) < make_pair(l.k, l.b);
     }
 };
 
@@ -32,22 +11,22 @@ const int LX = -1e5, RX = 1e5;
 
 struct LiChaoTree {
     struct Node {
-        Line li;
+        Line line;
         Node* l = nullptr;
         Node* r = nullptr;
 
-        Node(Line li_) : li(li_) {}
+        Node(Line l) : line(l) {}
     };
 
     Node* root = nullptr;
 
     int getMin(Node* t, int i, int lx, int rx) {
         if (!t) return LLONG_MAX;
-        int mx = div_ceil(lx + rx, 2);
+        int mx = (lx + rx) / 2;
         if (i < mx) {
-            return min(t->li(i), getMin(t->l, i, lx, mx - 1));
+            return min(t->line(i), getMin(t->l, i, lx, mx - 1));
         } else {
-            return min(t->li(i), getMin(t->r, i, mx, rx));
+            return min(t->line(i), getMin(t->r, i, mx, rx));
         }
     }
     int getMin(int i) {
@@ -58,14 +37,14 @@ struct LiChaoTree {
         if (!t) {
             t = new Node(a);
         } else {
-            int mx = div_ceil(lx + rx, 2);
-            if (t->li(mx) > a(mx)) {
-                swap(t->li, a);
+            int mx = (lx + rx) / 2;
+            if (t->line(mx) > a(mx)) {
+                swap(t->line, a);
             }
             if (lx < rx) {
-                if (a.k > t->li.k) {
+                if (a.k > t->line.k) {
                     t->l = insert(t->l, a, lx, mx - 1);
-                } else if (a.k < t->li.k) {
+                } else if (a.k < t->line.k) {
                     t->r = insert(t->r, a, mx, rx);
                 }
             }
