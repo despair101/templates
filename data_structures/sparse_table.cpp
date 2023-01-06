e6c05f34d9e9e22ed1f21f03ee622c29ea62ab61
Ilya Loginov <58655234+despair101@users.noreply.github.com>
2023-01-06 22:46:58 +0700

    Update sparse_table.cpp

--- data_structures/sparse_table.cpp
+++ data_structures/sparse_table.cpp
@@ -2,9 +2,13 @@ class SparseTable {
     vector<vector<pair<int, int>>> t;
 
 public:
-    int get(int l, int r) {
+    pair<int, int> get(int l, int r) {
         int pw = __lg(r - l);
-        return min(t[pw][l], t[pw][r - (1 << pw)]).second;
+        return compose(t[pw][l], t[pw][r - (1 << pw)]);
+    }
+    
+    pair<int, int> compose(const& pair<int, int> f, const& pair<int, int> s) {
+        return min(f, s);
     }
 
     explicit SparseTable() {}
@@ -16,7 +20,7 @@ public:
         }
         for (int j = 1; j <= __lg(n); ++j) {
             for (int i = 0; i + (1 << j) <= n; ++i) {
-                t[j][i] = min(t[j - 1][i], t[j - 1][i + (1 << (j - 1))]);
+                t[j][i] = compose(t[j - 1][i], t[j - 1][i + (1 << (j - 1))]);
             }
         }
     }
