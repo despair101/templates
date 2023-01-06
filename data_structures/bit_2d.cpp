2164f1b1770261281c498cfa58a1fbb15620c8d4
Ilya Loginov <58655234+despair101@users.noreply.github.com>
2023-01-06 22:08:34 +0700

    Create bit_2d.cpp

---
+++ data_structures/bit_2d.cpp
@@ -0,0 +1,27 @@
+class BIT_2D {
+    int size;
+    vector<vector<int>> t;
+
+    int get(int ind, int mi, int ma) {
+        int res = 0;
+        for (int i = ind; i >= 0; i &= i + 1, --i) {
+            res += upper_bound(t[i].begin(), t[i].end(), ma) - 
+                lower_bound(t[i].begin(), t[i].end(), mi);
+        }
+        return res;
+    }
+
+public:
+    int get(int l, int r, int mi, int ma) {
+        return get(r, mi, ma) - get(l - 1, mi, ma);
+    }
+
+    BIT_2D(const vector<int>& a) : size(a.size()), t(a.size()) {
+        for (int i = 0; i < size; ++i) {
+            for (int j = i; j < size; j |= j + 1) {
+                t[j].push_back(a[i]);
+            }
+            sort(t[i].begin(), t[i].end());
+        }
+    }
+};
