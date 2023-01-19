2d00db633c435572ff3895593d0021524b6acafd
Ilya Loginov <58655234+despair101@users.noreply.github.com>
2023-01-19 15:48:36 +0300

    Update hasher.cpp

--- data_structures/hasher.cpp
+++ data_structures/hasher.cpp
@@ -9,9 +9,7 @@ struct Hasher {
     }
     
     explicit Hasher() {}
-    explicit Hasher(const string& s) : size(s.size() + 1) {
-        h.assign(size, 0);
-        p.assign(size, 1);
+    explicit Hasher(const string& s) : size(s.size() + 1), h(size), p(size, 1) {
         for (int i = 0; i < size - 1; ++i) {
             h[i + 1] = h[i] * P + s[i];
             p[i + 1] = p[i] * P;
