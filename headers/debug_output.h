30f525a64ad8d7a83a990db1eaecaf40d2d4bb89
Ilya Loginov <58655234+despair101@users.noreply.github.com>
2023-01-27 16:10:33 +0700

    Update debug_output.h

--- headers/debug_output.h
+++ headers/debug_output.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "bits/stdc++.h"
+
 namespace debug {
     template<class A> void print(A a);
     template<class A, class B> void print(std::pair<A, B> a);
@@ -10,6 +12,7 @@ namespace debug {
     template<class A> void print(std::unordered_set<A> a);
     template<class A> void print(std::unordered_multiset<A> a);
     template<class A, class B> void print(std::unordered_map<A, B> a);
+    template<class A, size_t B> void print(std::array<A, B> a);
     template<class A> void print(std::vector<A> a);
     template<class A> void print(std::deque<A> a);
     template<class A> void print(A a) { std::cerr << a; }
@@ -18,7 +21,10 @@ namespace debug {
         std::cerr << "("; print(a.first); std::cerr << " "; print(a.second); std::cerr << ")";
     }
     template<class A, class B, class C> void print(std::tuple<A, B, C> a) {
-        std::cerr << '(' << get<0>(a) << ' ' << get<1>(a) << ' ' << get<2>(a) << ')';
+        std::cerr << '(' << std::get<0>(a) << ' ' << std::get<1>(a) << ' ' << std::get<2>(a) << ')';
+    }
+    template<class A, size_t B> void print(std::array<A, B> a) {
+        for (A b : a) { print(b); std::cerr << " "; }; std::cerr << "\n"; 
     }
     template<class A> void print(std::vector<A> a) {
         for (A b : a) { print(b); std::cerr << " "; }; std::cerr << "\n";
