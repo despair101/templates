3edf01ab3047b3a8af88e3f12a2ec1889b99dd90
Ilya Loginov <58655234+despair101@users.noreply.github.com>
2023-01-28 22:47:35 +0700

    Update segment_tree_2d.cpp

--- data_structures/segment_tree_2d.cpp
+++ data_structures/segment_tree_2d.cpp
@@ -1,8 +1,8 @@
 struct ST {
   int size;
-  vector<int> t;
+  std::vector<int> t;
  
-  void build(int x, int lx, int rx, const vector<int>& a) {
+  void build(int x, int lx, int rx, const std::vector<int>& a) {
     if (rx - lx == 1) {
       if (lx < size) {
         t[x] = a[lx];
@@ -11,36 +11,36 @@ struct ST {
       int mx = (lx + rx) / 2;
       build(2 * x + 1, lx, mx, a);
       build(2 * x + 2, mx, rx, a);
-      t[x] = min(t[2 * x + 1], t[2 * x + 2]);
+      t[x] = std::min(t[2 * x + 1], t[2 * x + 2]);
     }
   }
  
   int get(int l, int r, int x, int lx, int rx) {
-    if (lx >= r || rx <= l) return INF;
+    if (lx >= r || rx <= l) return INT_MAX;
     if (lx >= l && rx <= r) return t[x];
     int mx = (lx + rx) / 2;
-    return min(get(l, r, 2 * x + 1, lx, mx), get(l, r, 2 * x + 2, mx, rx));
+    return std::min(get(l, r, 2 * x + 1, lx, mx), get(l, r, 2 * x + 2, mx, rx));
   }
   int get(int l, int r) {
     return get(l, r, 0, 0, size);
   }
   
   ST() = default;
-  explicit ST(const vector<int>& a) : size(a.size()), t(4 * size) {
+  explicit ST(const std::vector<int>& a) : size(a.size()), t(4 * size) {
     build(0, 0, size, a);
   }
 };
  
 class MainSegmentTree {
   struct Node {
-    vector<int> v;
+    std::vector<int> v;
     ST st;
   };
   int size1, size2;
-  vector<Node> t;
+  std::vector<Node> t;
  
-  void build(int x, int lx, int rx, const vector<vector<int>>& a) {
-    t[x].v.assign(size2, INF);
+  void build(int x, int lx, int rx, const std::vector<std::vector<int>>& a) {
+    t[x].v.assign(size2, INT_MAX);
     if (rx - lx == 1) {
       if (lx < size1) {
         t[x].v = a[lx];
@@ -51,7 +51,7 @@ class MainSegmentTree {
       build(2 * x + 1, lx, mx, a);
       build(2 * x + 2, mx, rx, a);
       for (int i = 0; i < size2; ++i) {
-        t[x].v[i] = min(t[2 * x + 1].v[i], t[2 * x + 2].v[i]);
+        t[x].v[i] = std::min(t[2 * x + 1].v[i], t[2 * x + 2].v[i]);
       }
       t[x].st = ST(t[x].v);
     }
@@ -59,19 +59,19 @@ class MainSegmentTree {
  
 public:
   int get(int l1, int r1, int l2, int r2, int x, int lx, int rx) {
-    if (lx >= r1 || rx <= l1) return INF;
+    if (lx >= r1 || rx <= l1) return INT_MAX;
     if (lx >= l1 && rx <= r1) return t[x].st.get(l2, r2);
     int mx = (lx + rx) / 2;
-    return min(get(l1, r1, l2, r2, 2 * x + 1, lx, mx), get(l1, r1, l2, r2, 2 * x + 2, mx, rx));
+    return std::min(get(l1, r1, l2, r2, 2 * x + 1, lx, mx), get(l1, r1, l2, r2, 2 * x + 2, mx, rx));
   }
  
   int get(int l1, int r1, int l2, int r2) {
-    if (l1 >= r1 || l2 >= r2) return INF;
+    if (l1 >= r1 || l2 >= r2) return INT_MAX;
     return get(l1, r1, l2, r2, 0, 0, size1);
   }
   
   MainSegmentTree() = default;
-  explicit MainSegmentTree(const vector<vector<int>>& a) {
+  explicit MainSegmentTree(const std::vector<std::vector<int>>& a) {
     size1 = sz(a), size2 = sz(a[0]);
     t.resize(4 * size1);
     build(0, 0, size1, a);
