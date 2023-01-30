22cdaac6a616ed46ae8ed43757880a85b263266d
Ilya Loginov <58655234+despair101@users.noreply.github.com>
2023-01-30 23:02:16 +0700

    Update fenwick_tree_2d.cpp

--- data_structures/fenwick_tree_2d.cpp
+++ data_structures/fenwick_tree_2d.cpp
@@ -12,7 +12,7 @@ class FenwickTree2D {
         return res;
     }
 
-public:
+ public:
     int get(int l, int r, int mi, int ma) {
         if (l > r) return 0;
         return get(r, mi, ma) - get(l - 1, mi, ma);
