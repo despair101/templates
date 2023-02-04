05f9f351048600290159eebc9a04d39f91e8ea0a
Ilya Loginov <58655234+despair101@users.noreply.github.com>
2023-02-04 12:25:40 +0700

    Update gcc_functions.h

--- headers/gcc_functions.h
+++ headers/gcc_functions.h
@@ -1,6 +1,9 @@
 #pragma once
 
-int __builtin_popcount(int x) {
+#include <cassert>
+
+inline int __builtin_popcount(int x) {
+    assert(x >= 0);
     int cnt = 0;
     while (x) {
         cnt += (x & 1);
@@ -9,7 +12,8 @@ int __builtin_popcount(int x) {
     return cnt;
 }
 
-int __builtin_popcountll(long long x) {
+inline int __builtin_popcountll(long long x) {
+    assert(x >= 0);
     int cnt = 0;
     while (x) {
         cnt += (x & 1);
@@ -18,39 +22,44 @@ int __builtin_popcountll(long long x) {
     return cnt;
 }
 
-int __builtin_ctz(int x) {
+inline int __builtin_ctz(int x) {
+    assert(x >= 0);
     int cnt = 0;
-    for (int i = 0; i <= 31; ++i, ++cnt) {
-        if (1 & (x >> i)) break;
+    for (int i = 0; i < 31; ++i, ++cnt) {
+        if (x >> i & 1) break;
     }
     return cnt;
 }
 
-int __builtin_ctzll(long long x) {
+inline int __builtin_ctzll(long long x) {
+    assert(x >= 0);
     int cnt = 0;
-    for (int i = 0; i <= 63; ++i, ++cnt) {
-        if (1 & (x >> i)) break;
+    for (int i = 0; i < 63; ++i, ++cnt) {
+        if (x >> i & 1) break;
     }
     return cnt;
 }
 
-int __builtin_clz(int x) {
+inline int __builtin_clz(int x) {
+    assert(x >= 0);
     int cnt = 0;
     for (int i = 31; i >= 0; --i, ++cnt) {
-        if (1 & (x >> i)) break;
+        if (x >> i & 1) break;
     }
     return cnt;
 }
 
-int __builtin_clzll(long long x) {
+inline int __builtin_clzll(long long x) {
+    assert(x >= 0);
     int cnt = 0;
     for (int i = 63; i >= 0; --i, ++cnt) {
-        if (1 & (x >> i)) break;
+        if (x >> i & 1) break;
     }
     return cnt;
 }
 
-int __lg(long long x) {
+inline int __lg(long long x) {
+    assert(x >= 0);
     int cnt = 0;
     for (int i = 1; i < 63; ++i, ++cnt) {
         if ((1ll << i) > x) break;
