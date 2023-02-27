4e696b49429738135b805c33662fa4a3ecadc98c
Ilya Loginov <58655234+despair101@users.noreply.github.com>
2023-02-27 09:27:59 +0300

    Update based.cpp

--- based.cpp
+++ based.cpp
@@ -20,13 +20,11 @@ bool umax(A& x, A y) { if (y > x) { x = y; return true; } return false; }
 template<class A>
 bool umin(A& x, A y) { if (y < x) { x = y; return true; } return false; }
 
-std::mt19937 rnd(101);
-
 typedef long long ll;
 typedef long double ld;
 
 // #define int int64_t
-#define sz(x) int(x.size())
+#define sz(x) int((x).size())
 
 void solve() {
 }
