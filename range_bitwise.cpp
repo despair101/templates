5a7e119bbf8c26c72284ccd8dfac17b15cee7485
Ilya Loginov <58655234+despair101@users.noreply.github.com>
2023-08-14 01:03:44 +0200

    Update range_bitwise.cpp
    Permitted range for l, r was added

--- range_bitwise.cpp
+++ range_bitwise.cpp
@@ -1,1 +1,37 @@
+// works for 1 <= l <= r <= 2^63-1
 
+ll range_xor(ll l, ll r) {
+    if (l == 1) {
+        if (r % 4 == 0) {
+            return r;
+        } else if (r % 4 == 1) {
+            return 1;
+        } else if (r % 4 == 2) {
+            return r + 1;
+        } else {
+            return 0;
+        }
+    }
+
+    return range_xor(1, r) ^ range_xor(1, l - 1);
+}
+
+ll range_and(ll l, ll r) {
+    ll res = 0;
+    for (int i = 0; i <= __lg(l); ++i) {
+        if ((l >> i & 1) && r / (1 << i) == l / (1 << i)) {
+            res += (1 << i);
+        }
+    }
+    return res;
+}
+
+ll range_or(ll l, ll r) {
+    ll res = 0;
+    for (int i = 0; i <= __lg(r); ++i) {
+        if ((l >> i & 1) || r / (1 << i) != l / (1 << i)) {
+            res += (1 << i);
+        }
+    }
+    return res;
+}
