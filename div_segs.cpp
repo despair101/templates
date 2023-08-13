91f3179451675a28d3fdb9a7d383bfb761181a11
Ilya Loginov <58655234+despair101@users.noreply.github.com>
2023-08-14 01:07:06 +0200

    Update div_segs.cpp
    tuple<int, int, int> -> array<int, 3>

--- div_segs.cpp
+++ div_segs.cpp
@@ -7,26 +7,26 @@ int div_ceil(int a, int b) {
 }
 
 // floor(n / l) = x   =>   rx <= n   =>   r = floor(n / x)
-vector<tuple<int, int, int>> get_floors(int n) {
-    vector<tuple<int, int, int>> res;
+vector<array<int, 3>> get_floors(int n) {
+    vector<array<int, 3>> res;
     int l = 1;
     while (l <= n) {
         int x = div_floor(n, l);
         int r = div_floor(n, x);
-        res.emplace_back(l, r, x);
+        res.push_back({l, r, x});
         l = r + 1;
     }
     return res;
 }
 
 // ceil(n / l) = x   =>   r(x - 1) < n   =>   r = ceil(n, x - 1) - 1
-vector<tuple<int, int, int>> get_ceils(int n) {
-    vector<tuple<int, int, int>> res;
+vector<array<int, 3>> get_ceils(int n) {
+    vector<array<int, 3>> res;
     int l = 1;
     while (l <= n) {
         int x = div_ceil(n, l);
         int r = (x - 1 > 0 ? div_ceil(n, x - 1) - 1 : n);
-        res.emplace_back(l, r, x);
+        res.push_back({l, r, x});
         l = r + 1;
     }
     return res;
