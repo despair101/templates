07bdfc8935ac4e9b6aecdf8b5c5cba1e186249da
Ilya Loginov <58655234+despair101@users.noreply.github.com>
2023-10-15 00:24:13 +0300

    Update based.cpp

--- based.cpp
+++ based.cpp
@@ -2,7 +2,10 @@
 
 #ifdef DESPAIR_101
 #include "../debug_output.h"
-#define dbg(x) cerr << "\n" << #x << ":\n "; debug::print(x); cerr << "\n"
+#define dbg(x)                              \
+    cerr << "\033[2;31m\n" << #x << ":\n "; \
+    debug::print(x);                        \
+    cerr << "\033[0m\n"
 #define showTest(x) cerr << "_____Test #" << x << "_____\n"
 #else
 #define dbg(x)
@@ -15,19 +18,32 @@ using ll = long long;
 using ld = long double;
 // #define int ll
 
-template <class A> inline int sz(A const &x) { return int(x.size()); }
-template <class A> inline bool umin(A &x, A const &y) { return x > y ? (x = y, true) : false; }
-template <class A> inline bool umax(A &x, A const &y) { return x < y ? (x = y, true) : false; }
+template <class A>
+inline int sz(A const& x) {
+    return int(x.size());
+}
+template <class A>
+inline bool umin(A& x, A const& y) {
+    return x > y ? (x = y, true) : false;
+}
+template <class A>
+inline bool umax(A& x, A const& y) {
+    return x < y ? (x = y, true) : false;
+}
 
 void solve() {}
 
 signed main() {
+#ifdef DESPAIR_101
+    freopen("inputik.txt", "r", stdin);
+#endif
+
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
     cout << fixed << setprecision(20);
 
     int testsCnt = 1;
-    // cin >> testsCnt;
+    cin >> testsCnt;
     for (int curTest = 1; curTest <= testsCnt; ++curTest) {
         showTest(curTest);
         solve();
