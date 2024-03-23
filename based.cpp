cb329436d4e553d37171fcc289fa17f2a8cd6708
Ilya Loginov <58655234+despair101@users.noreply.github.com>
2024-03-23 23:42:31 +0300

    Update based.cpp

--- based.cpp
+++ based.cpp
@@ -15,19 +15,20 @@
 using namespace std;
 
 using ll = long long;
+using ull = unsigned long long;
 using ld = long double;
 // #define int ll
 
 template <class A>
-inline int sz(A const& x) {
+inline int sz(const A& x) {
     return int(x.size());
 }
 template <class A>
-inline bool umin(A& x, A const& y) {
+inline bool umin(A& x, const A& y) {
     return x > y ? (x = y, true) : false;
 }
 template <class A>
-inline bool umax(A& x, A const& y) {
+inline bool umax(A& x, const A& y) {
     return x < y ? (x = y, true) : false;
 }
 
@@ -37,13 +38,12 @@ signed main() {
 #ifdef DESPAIR_101
     freopen("inputik.txt", "r", stdin);
 #endif
-
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
     cout << fixed << setprecision(20);
 
     int testsCnt = 1;
-    cin >> testsCnt;
+    // cin >> testsCnt;
     for (int curTest = 1; curTest <= testsCnt; ++curTest) {
         showTest(curTest);
         solve();
